01f698397ee3cec0c02a08b794ca773b334ba7fe
Tom Stoll <Thomas.Stoll@dartmouth.edu>
2013-11-02 20:10:42 +0000

    Slight update to __init__ copyright

--- action/__init__.py
+++ action/__init__.py
@@ -1,8 +1,8 @@
 # ACTION - python toolkit for cinematic information retrieval (bregman.dartmouth.edu/~action)
 
 __version__ = '1.0'
-__author__ = 'Thomas M. Stoll'
-__copyright__ = "Copyright (C) 2012-2013  Thomas Stoll, Michael Casey, Dartmouth College, All Rights Reserved"
+__author__ = 'Thomas M. Stoll, Michael Casey'
+__copyright__ = "Copyright (C) 2012-2013, Trustees of Dartmouth College, All Rights Reserved"
 __license__ = "gpl 2.0 or higher"
 __email__ = 'thomas.m.stoll@dartmouth.edu'
 
