08c6d185e7e945960a2321fe1281382666d4d722
Tom Stoll <Thomas.M.Stoll@dartmouth.edu>
2014-01-11 15:01:49 -0500

    Fixing up autogen script for interactive site

--- web/autogen_filmdb_individual_textfiles.py
+++ web/autogen_filmdb_individual_textfiles.py
@@ -17,7 +17,7 @@ print len(titles)
 html_filestring = os.path.expanduser('~/Sites/action_db.html')
 htmlfile = open(html_filestring, 'w')
 
-htitles, clinks, alinks, dirs, years, colors, lengths, hashes = [], [], [], [], [], [], [], []
+htitles, clinks, alinks, calinks, dirs, years, colors, lengths, hashes = [], [], [], [], [], [], [], [], []
 
 print os.path.join(ACTION_DIR, '*', '*.color_lab')
 
@@ -36,15 +36,21 @@ for file in glob.glob(os.path.join(ACTION_DIR, '*/*.color_lab')):
 		if not os.path.exists(os.path.join(WEBDIR,hashstr)):
 			os.makedirs(os.path.join(WEBDIR,hashstr))
 
-		audioflag = 0
 		# Copy the TITLE.*_hc_*.pkl files from ACTION_DIR to WEBDIR/hash		
 		shutil.copy2((os.path.join(ACTION_DIR,str(ttl),(str(ttl)+"_cfl_hc.pkl"))), (os.path.join(WEBDIR,hashstr,(str(ttl)+"_cfl_hc.pkl"))))
-		shutil.copy2((os.path.join(ACTION_DIR,str(ttl),(str(ttl)+"_cfl_hc.pkl"))), (os.path.join(WEBDIR,hashstr,(str(ttl)+"_cfl_hc.pkl"))))
+		audioflag = 0
 		try:
 			shutil.copy2((os.path.join(ACTION_DIR,str(ttl),(str(ttl)+"_mfccs_hc.pkl"))), (os.path.join(WEBDIR,hashstr,(str(ttl)+"_mfccs_hc.pkl"))))
 			audioflag = 1
 		except IOError:
 			pass
+		comboflag = 0
+		try:
+			shutil.copy2((os.path.join(ACTION_DIR,str(ttl),(str(ttl)+"_combo_hc.pkl"))), (os.path.join(WEBDIR,hashstr,(str(ttl)+"_combo_hc.pkl"))))
+			comboflag = 1
+		except IOError:
+			pass
+
 		
 		# Create the text file for the metadata
 		txtfile = os.path.join(WEBDIR, hashstr, (str(ttl) + '.txt'))
@@ -59,6 +65,11 @@ for file in glob.glob(os.path.join(ACTION_DIR, '*/*.color_lab')):
 			f.write(HTML.link("AUDIO FEATURES", ("film_detail_audio.php?hash=" + hashstr + "&t=30&mf=20&g=60")) + "\n")
 		else:
 			f.write("n/a\n")
+		if comboflag == 1:
+			f.write(HTML.link("COMBINED FEATURES", ("film_detail_combo.php?hash=" + hashstr + "&t=30&mf=20&g=60")) + "\n")
+		else:
+			f.write("n/a\n")
+
 		print str(actionDB[ttl][1])
 		f.write(str(actionDirectors[ str(actionDB[ttl][1]) ][0] ) + "\n")
 		f.write(str(actionDB[ttl][3]) + "\n")
@@ -76,11 +87,15 @@ for file in glob.glob(os.path.join(ACTION_DIR, '*/*.color_lab')):
 # 		print 'ttl: ', ttl
 		htitles += [str(actionDB[ttl][0]).replace("_"," ")]
 
-		clinks += [HTML.link("COLOR FEATURES", ("film_detail.php?hash=" + hashstr + "&t=30&mf=20&g=60"))]
+		clinks += [HTML.link("COLOR FEATURES", ("film_detail.php?hash=" + hashstr + "&t=30&mf=20&g=30"))]
 		if audioflag == 1:
-			alinks += [HTML.link("AUDIO FEATURES", ("film_detail_audio.php?hash=" + hashstr + "&t=30&mf=20&g=60"))]
+			alinks += [HTML.link("AUDIO FEATURES", ("film_detail_audio.php?hash=" + hashstr + "&t=30&mf=20&g=30"))]
 		else:
 			alinks += ["n/a"]
+		if comboflag == 1:
+			calinks += [HTML.link("COMBINED FEATURES", ("film_detail_comboo.php?hash=" + hashstr + "&t=30&mf=20&g=30"))]
+		else:
+			calinks += ["n/a"]
 		dirs += [actionDirectors[ str(actionDB[ttl][1]) ][0] ]
 		years += [str(actionDB[ttl][3])]
 		colors += [cflag]
@@ -90,13 +105,15 @@ for file in glob.glob(os.path.join(ACTION_DIR, '*/*.color_lab')):
 
 print len(clinks)
 print len(alinks)
-full_table = [["Title", "Color Seg.", "Audio Seg.", "Director", "Year", "Color", "Length (s)", "Hash"]]
+print len(calinks)
+full_table = [["Title", "Color Seg.", "Audio Seg.", "Combo Seg.", "Director", "Year", "Color", "Length (s)", "Hash"]]
 
 for i, t in enumerate(clinks):
 	full_table += [[
 		htitles[i],
 		clinks[i],
 		alinks[i],
+		calinks[i],
 		dirs[i],
 		years[i],
 		colors[i],
