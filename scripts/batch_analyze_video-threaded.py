101d3221a72a8ddb359760dcb6cf32aff2fe4a05
Tom Stoll <Thomas.M.Stoll@dartmouth.edu>
2014-02-21 16:58:19 -0500

    Cleaning up some naming conventions and default command line arguments.

--- scripts/batch_analyze_video-threaded.py
+++ scripts/batch_analyze_video-threaded.py
@@ -1,33 +1,32 @@
 import glob, os, argparse
-import bregman.segment as seg
+import multiprocessing
 from action import *
 import action
 
-import multiprocessing
 
 ACTIONDIR = '/Volumes/ACTION'
+NUM_PROCS = 4 # This is how many processes we want
 
-PROC_LIMIT = 4                # This is how many processes we want
-
-
-def actionAnalyzeAll(hlist, plist, olist):
+def actionAnalyzeAll(clist, plist, olist):
 	print "Inputlist received..."
-	print hlist
+	print clist
 	print plist
 	print olist
 	# create the pool
-	pool = multiprocessing.Pool(PROC_LIMIT)
-	#pool.map(actionHistWorker, hlist)
-	#pool.map(actionPCorrWorker, plist)
+	pool = multiprocessing.Pool(NUM_PROCS)
+	
+	# COMMENT OUT LINES HERE TO SKIP ANALYSIS TYPES
+	pool.map(actionHistWorker, clist)
+	pool.map(actionPCorrWorker, plist)
 	pool.map(actionOFlowWorker, olist)
 
 
-def actionHistWorker(hfile):
-	hist = action.color_features_lab.ColorFeaturesLAB(hfile, action_dir=ACTIONDIR)
-	print 'analyzing colors: ', (hfile + '.mov'), ' ', (hfile + '.color_lab')
+def actionHistWorker(cfile):
+	hist = action.color_features_lab.ColorFeaturesLAB(cfile, action_dir=ACTIONDIR)
+	print 'analyzing colors: ', (hfile + '.mov'), ' ', (cfile + '.color_lab')
 	print 'action_dir=/Volumes/ACTION'
 	hist.analyze_movie()
-	print 'DONE analyzing color features: ', (hfile + '.mov'), ' ', (hfile + '.color_lab')
+	print 'DONE analyzing color features: ', (cfile + '.mov'), ' ', (cfile + '.color_lab')
 	return 1
 
 def actionPCorrWorker(pfile):
@@ -53,25 +52,30 @@ if __name__ == '__main__':
 
 	print args
 	print args.actiondir
+	
+	if args.actiondir is not None:
+		ACTIONDIR = args.actiondir
+	if args.numprocs is not None:
+		NUM_PROCS = args.numprocs
 
 	# if we have some args, use them
 
-	os.chdir(args.actiondir)
+	os.chdir(actiondir)
 	names = [os.path.dirname(file) for file in glob.glob('*/*.mov')]
 	names_with_proper_hist_exts = glob.glob('*/*.color_lab')
-	just_names_of_hists = [os.path.dirname(file) for file in names_with_proper_hist_exts]
+	just_names_of_cflabs = [os.path.dirname(file) for file in names_with_proper_cflab_exts]
 	names_with_proper_pcorr_exts = glob.glob('*/*.phasecorr')
 	just_names_of_pcorrs = [os.path.dirname(file) for file in names_with_proper_pcorr_exts]
 	names_with_proper_oflow_exts = glob.glob('*/*.opticalflow24')
 	just_names_of_oflows = [os.path.dirname(file) for file in names_with_proper_oflow_exts]
 
-	hists_to_analyze = set(names).difference(set(just_names_of_hists))
+	cflabs_to_analyze = set(names).difference(set(just_names_of_cflabs))
 	pcorrs_to_analyze = set(names).difference(set(just_names_of_pcorrs))
 	oflows_to_analyze = set(names).difference(set(just_names_of_oflows))
 
 	print ''
-	print hists_to_analyze
-	print "(", len(hists_to_analyze), ")"
+	print cflabs_to_analyze
+	print "(", len(cflabs_to_analyze), ")"
 	print ''
 	print pcorrs_to_analyze
 	print "(", len(pcorrs_to_analyze), ")"
@@ -81,4 +85,4 @@ if __name__ == '__main__':
 	print ''
 
 	# Call the mainfunction that sets up threading.
-	actionAnalyzeAll(hists_to_analyze, pcorrs_to_analyze, oflows_to_analyze)
+	actionAnalyzeAll(cflabs_to_analyze, pcorrs_to_analyze, oflows_to_analyze)
