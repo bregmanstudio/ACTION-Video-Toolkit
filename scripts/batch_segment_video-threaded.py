cee96b7dd836a10600f33694dd7baaeb96a2eacd
Tom Stoll <Thomas.M.Stoll@dartmouth.edu>
2014-03-21 13:00:03 -0400

    Minor updates, whitespace and comments.

--- scripts/batch_segment_video-threaded.py
+++ scripts/batch_segment_video-threaded.py
@@ -1,13 +1,9 @@
 import glob, os, argparse
 import pickle, multiprocessing
-
 import numpy as np
-
 from action import *
 import action.segment as aseg
 from action.actiondata import *
-
-
 #from bregman.suite import *
 
 ACTION_DIR = '/Volumes/ACTION/'
@@ -20,48 +16,30 @@ def actionAnalyzeAll(inputlist, proc_limit):
 	pool = multiprocessing.Pool(proc_limit)
 	pool.map(actionWorker, inputlist)
 
-
+# print statements will alert user to any inconsistencies
 def actionWorker(title):
-	
 	ad = actiondata.ActionData()
 	ds_segs_mb, ds_segs_mfccs, ds_segs_combo = [], [], []
-#	actual_fps = color_features_lab.ColorFeaturesLAB(title, action_dir=ACTION_DIR)._read_json_value('fps')
-#	print ':: ', actual_fps
-	# reinstantiate cfl with the proper fps for access
 	cfl = color_features_lab.ColorFeaturesLAB(title, action_dir=ACTION_DIR) #, fps=actual_fps)
-
 	print '----------------------------------'
 	print title
 	print ''
-
 	length = cfl.determine_movie_length() # in seconds
 	length_in_frames = length * 4
-
 	full_segment = aseg.Segment(0, duration=length)
 	Dmb = cfl.middle_band_color_features_for_segment(full_segment)
 	# if abFlag is True: Dmb = cfl.convertLabToL(Dmb)
-	
+	Dmb = ad.meanmask_data(Dmb)
 	Dmfccs = ad.meanmask_data(adb.read(os.path.join(ACTION_DIR, title, (title + '.mfcc'))))
 	Dmfccs = ad.normalize_data(Dmfccs)
 	Dmfccs = ad.meanmask_data(Dmfccs)
-	
-	# print Dmb.shape
-	print Dmfccs.shape
-	print Dmfccs.min()
-	print Dmfccs.max()
-	
-	# print np.isnan(Dmb).any()
-	print np.isnan(Dmfccs).any()
-	# print np.isnan(Dcombo).any()
-	
+	print [Dmb.shape, Dmb.min(), Dmb.max(), np.isnan(Dmb).any()]
+	print [Dmfccs.shape, Dmfccs.min(), Dmfccs.max(), np.isnan(Dmfccs).any()]
 	min_length = min(Dmb.shape[0], Dmfccs.shape[0])
-	
 	Dcombo = np.c_[Dmb[:min_length,:], Dmfccs[:min_length,:]]
-	
 	nc = length_in_frames / 10
 	print nc
 	print "----------------------------"
-
 	if not np.isnan(Dmb).any():
 		outfile_mb = open(os.path.expanduser(os.path.join(ACTION_DIR, title, (title+'_cfl_hc.pkl'))), 'wb')
 		decomposed_mb = ad.calculate_pca_and_fit(Dmb, locut=0.0001)
@@ -76,7 +54,6 @@ def actionWorker(title):
 				features=np.mean(Dmb[seg[0]:(seg[0]+seg[1]),:],axis=0))]
 		pickle.dump(ds_segs_mb, outfile_mb, -1)
 		outfile_mb.close()
-	
 	if not np.isnan(Dmfccs).any():
 		outfile_mfccs = open(os.path.expanduser(os.path.join(ACTION_DIR, title, (title+'_mfccs_hc.pkl'))), 'wb')
 		decomposed_mfccs = ad.calculate_pca_and_fit(Dmfccs, locut=0.001)
@@ -91,7 +68,6 @@ def actionWorker(title):
 				features=np.mean(Dmfccs[seg[0]:(seg[0]+seg[1]),:],axis=0))]
 		pickle.dump(ds_segs_mfccs, outfile_mfccs, -1)
 		outfile_mfccs.close()
-	
 	if not np.isnan(Dcombo).any():
 		outfile_combo = open(os.path.expanduser(os.path.join(ACTION_DIR, title, (title+'_combo_hc.pkl'))), 'wb')
 		decomposed_combo = ad.calculate_pca_and_fit(Dcombo, locut=0.0001)	
@@ -106,10 +82,8 @@ def actionWorker(title):
 				features=np.mean(Dcombo[seg[0]:(seg[0]+seg[1]),:],axis=0))]
 		pickle.dump(ds_segs_combo, outfile_combo, -1)
 		outfile_combo.close()
-	
 	# 	sliding_averaged = ad.average_over_sliding_window(decomposed, 8, 4, length_in_frames)
 	# 	hc_assigns = ad.cluster_hierarchically(sliding_averaged, nc, None)
-	
 	# clean up
 	del Dmb
 	del Dmfccs
